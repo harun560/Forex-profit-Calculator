60a4332495012ae008c1feec0205ec43ddafd42d
harun560 <harun560@yahoo.com>
2023-04-23 16:18:14 -0500

    Update index.js
    Adding incremental changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-  let openprice, onePip, pipValue, volume, pipV, takeProfitLevel,tp; 
+  let openprice, onePip, pipValue, volume, pipV, takeProfitLevel,tp, takeLossLevel, tL; 
   onePip = 0.0001;
   function pipUsd(){
     volume = document.getElementById("four").value;
@@ -16,9 +16,17 @@
     pipValue =(onePip/ openprice)*volume;
     tp = (takeProfitLevel/pipValue)*volume;
     document.getElementById("first").textContent = tp;
-
-    
-    
   }
+
+function takeLoss() {
+    volume = document.getElementById("four").value;
+     openprice = document.getElementById("five").value;
+  takeLossLevel = document.getElementById("seven").value;
+    pipValue =(onePip/ openprice)*volume;
+  tL = (takeLossLevel/pipValue)*volume;
+  document.getElementById("second").textContent = tL;
+  
+  
+}
 
 
